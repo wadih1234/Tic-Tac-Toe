e593f918ce143e0862437fb8f0d068aac34a41dc
wadih1234 <69472399+wadih1234@users.noreply.github.com>
2021-05-31 12:13:11 +0300

    Add files via upload

---
+++ tic-tac-toe/connection.php
@@ -0,0 +1,9 @@
+<?php
+/* these variables will use for connection to mysql across all pages */
+$server = "localhost";
+$username = "wadih";
+$password = "\ZfF3n0)XA\1>=jf";
+$dbname = "id13308999_tictactoe";
+
+
+?>
